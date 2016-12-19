77dc8d77305233f4334e9ae5109132c9123fcc57
Sardred <andelar96@gmail.com>
2016-12-19 21:03:42 +0100

    Style changes

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import {amber500, blueGrey500, blueGrey700} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Wip from './components/wip';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Main from './components/main';
+import Header from './components/header'
+
+
+// Needed for onTouchTap()
+injectTapEventPlugin();
+
+
+// Changing MuiTheme colors
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blueGrey500,
+    primary2Color: blueGrey700,
+    accent1Color: amber500,
+    pickerHeaderColor: blueGrey500
+  }
+});
 
 const App = () => (
-  <MuiThemeProvider>
-    <Wip />
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div>
+      <Header />
+      <Main />
+    </div>
   </MuiThemeProvider>
 );
 
