c9b2b981ee27b390855952d553af79b817333644
Sardred <andelar96@gmail.com>
2016-12-19 21:04:34 +0100

    Added main component

---
+++ components/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  width: "75%",
+  margin: "0 auto"
+};
+
+const Main = () => (
+  <Paper zDepth={2} style={style}>
+  </Paper>
+);
+
+export default Main;
