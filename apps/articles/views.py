b9c91b6f3f50c47b43df1eb70e3c85aa546051a7
Sardred <andelar96@gmail.com>
2016-12-26 00:22:11 +0100

    Added ViewSet for the articles, and a potentially painfully mediocre view for details

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from apps.articles.models import Article
+from apps.articles.serializers import ArticleSerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by('-date_joined')
+    serializer_class = ArticleSerializer
+
+
+def details(request, article_id, article_slug):
+
+    # Get the article based on its id
+    article = get_object_or_404(Article, pk=article_id)
+
+    return render(request, 'articles/details.html', {'article': article})
