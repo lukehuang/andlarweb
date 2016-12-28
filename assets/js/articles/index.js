71f2c19d083bb2f595d3a8f001dc4c245ed3355b
Sardred <andelar96@gmail.com>
2016-12-28 01:25:07 +0100

    Skipped a redundant component step, requiring polyfill and fetch

--- assets/js/articles/index.js
+++ assets/js/articles/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { AW_THEME } from "../common/constants";
-import Main from "./components/main";
+import {AW_THEME} from "../common/constants";
+import ArticlesContainer from "./components/articles";
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-const MainContainer = () => (
+const Main = () => (
   <MuiThemeProvider muiTheme={AW_THEME}>
-    <Main />
+    <ArticlesContainer />
   </MuiThemeProvider>
 );
 
 ReactDOM.render(
-  <MainContainer />,
+  <Main />,
   document.getElementById('main')
 );
