9365806d349193c4a446699196a137c662c13bdf
Sardred <andelar96@gmail.com>
2016-12-28 01:28:26 +0100

    Added key that was totally there the whole time, and styled the div holding the articles

--- assets/js/articles/components/articles/articles.js
+++ assets/js/articles/components/articles/articles.js
@@ -1,11 +1,17 @@
 import React, {Component} from "react";
 import Article from "./article";
 
+
+
 const Articles = props => (
-  <div>
+  <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      padding: '32px 0 16px',
+    }}>
     {
-      props.articles.map(article =>
-        <Article article={article} />
+      props.articles.map((article, index) =>
+        <Article article={article} key={index} />
       )
     }
   </div>
