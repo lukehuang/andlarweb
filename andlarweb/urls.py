a40ae42ffdc673c9881591dc6f0703b547c83d13
Sardred <andelar96@gmail.com>
2017-01-01 22:07:05 +0100

    Added urls for media and static in development

--- andlarweb/urls.py
+++ andlarweb/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from apps.articles import views as articles_views
 
@@ -14,6 +15,11 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # Add apps to urlpatterns
 if 'apps.api' in settings.INSTALLED_APPS:
